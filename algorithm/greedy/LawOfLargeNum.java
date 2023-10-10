c414360d3bcd36ac44d3c0b9b0da7e66a44049da
TakGyun <gyun1712@gmail.com>
2023-10-10 14:33:25 +0900

    greedy 문제 해결법 작성

--- algorithm/greedy/LawOfLargeNum.java
+++ algorithm/greedy/LawOfLargeNum.java
@@ -3,6 +3,12 @@ package algorithm.greedy;
 import java.util.*;
 import java.io.*;
 
+/**
+ * greedy 알고리즘을 이용한 기본 문제
+ * 주의점 : 탐욕적 방식을 통해 정확한 답을 찾는다는 보장이 있을 떄 효과적
+ *        따라서 해법이 정당한지 검토하는 과정이 필요하다.
+ *  검증 : 반복되는 수열을 통해 가장 큰 수와 두 번째로 큰 수가 등장하는 횟수를 파악한다.
+ */
 public class LawOfLargeNum {
 
     static int n;
