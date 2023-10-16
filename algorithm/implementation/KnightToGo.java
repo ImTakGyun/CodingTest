13066e540da93561bbda73e864c1c8be6bae0c31
TakGyun <gyun1712@gmail.com>
2023-10-16 23:33:03 +0900

    feat: 왕실 나이트 문제 풀이 해설

--- algorithm/implementation/KnightToGo.java
+++ algorithm/implementation/KnightToGo.java
@@ -6,14 +6,16 @@ import java.io.*;
 // 2차원 배열이 사용되는 전형적인 구현 문제
 public class KnightToGo {
     // 해결 아이디어 => 구현 문제라서 조건에 충실하면 됨
-    // 오답 => 입력 받은 String 문자열을 charAt() 함수를 통해 문자를 분리해서 받아올 수 있음을 적용하지 못 함.
+    // 어려웠던 점 => 입력 받은 String에서 charAt() 함수를 통해 문자를 분리하여 받아올 수 있음을 몰랐음.
 
     static int n = 8;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
-        Scanner sc = new Scanner(System.in);
-        String inputData = sc.next();
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(r.readLine());
+
+        String inputData = st.nextToken();
 
         // 현재 나이트의 위치 입력받기 => String 의 charAt()을 통해 문자를 분리하여 원하는 위치값을 변환
         int row = inputData.charAt(1) - '0';
@@ -27,8 +29,8 @@ public class KnightToGo {
         int result = 0;
         for (int i = 0; i < 8; i++){
             //이동하고자 하는 위치 확인
-            int nextRow = row + dx[i];
-            int nextColumn = column + dy[i];
+            int nextRow = row + dy[i];
+            int nextColumn = column + dx[i];
 
             if(nextRow >= 1 && nextRow <= n && nextColumn >= 1 && nextColumn <= n)
                 result++;
