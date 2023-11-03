7da3976d0f2b8346b665c0897b6de0d5001accd2
TakGyun <gyun1712@gmail.com>
2023-11-03 21:21:13 +0900

    feat: 다이나믹 프로그래밍-주어진 화폐 단위를 이용한 값의 최소 구성 풀이 및 해설

---
+++ algorithm/DynamicProgramming/Practice/실전_효율적인화폐구성.java
@@ -0,0 +1,58 @@
+package algorithm.DynamicProgramming.Practice;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ *  핵심 Point : 처음에 나는 점화식을 세우기 위해서 규칙성을 찾고 있었다.
+ *              특정 수 (a)를 만들 때 (a-k)가 구성되는 최소한의 화폐 개수에 +1(K원짜리 화폐)한다는 아이디어까지는 생각해냈다.
+ *              하지만 주어진 화폐의 종류에 따라 순차적으로 값에 대한 최소한의 화폐 구성을 update 하지 않고
+ *              주어진 값에 모든 화폐 종류에 따른 if문을 통해 구현하려다보니 복잡해지고 어려워졌다.
+ *
+ *              이를 해결하기 위해서는 주어진 화폐 단위를 최소부터 이용하여 반복문을 통해 해당 화페가 마지막으로 추가되는 경우에 대한 최소 화폐의 개수를 구한다.
+ *              이를 통해 주어진 화폐의 단위를 이용하여 각각의 값에 대해 최소한으로 구성하는 화폐 개수를 update 해나가야 한다.
+ *              cnt을 10001로 초기화한 것은 만들어질 수 없는 값을 의미하며, 최소한으로 구성되는 조건을 만족시키기 위해 INF 값의 의미이다.
+ *              2 15 2 3 을 가정할 때, 최소 화폐 단위인 2로 cnt에 값이 생성되는 최소한의 개수를 삽입하고
+ *              3으로 다시 cnt를 사용하여 최소 개수를 삽입한다.
+ */
+public class 실전_효율적인화폐구성 {
+
+    public void solution()throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+        int[] coin = new int[n];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            coin[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(coin);
+
+        int[] cnt = new int[m+1];
+        Arrays.fill(cnt, 10001);
+
+        cnt[0] = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = coin[i]; j < m+1; j++){
+                // (i-k)원을 만드는 방법이 존재하는 경우
+                if(cnt[j - coin[i]] != 10001){
+                    cnt[j] = Math.min(cnt[j], cnt[j -coin[i]] + 1);
+                }
+            }
+        }
+
+        if(cnt[m] == 10001) System.out.println(-1);
+        else System.out.println(cnt[m]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new 실전_효율적인화폐구성().solution();
+    }
+
+}
