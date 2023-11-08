662513563a6ec4f183e8927c1d2891798251772a
TakGyun <gyun1712@gmail.com>
2023-11-08 23:31:35 +0900

    feat: SWEA-단순 구현,SUM

---
+++ SWEA/D3/SW_1209.java
@@ -0,0 +1,60 @@
+package SWEA.D3;
+
+import java.util.*;
+import java.io.*;
+
+public class SW_1209 {
+
+    public static int[][] arr;
+
+    public static void main(String args[]) throws Exception
+    {
+        int T = 10;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            br.readLine();
+
+            StringTokenizer st;
+
+            arr = new int[100][100];
+
+            for(int i = 0; i < 100; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < 100; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+            int cross = 0;
+
+            for(int i = 0; i < 100; i++){
+
+                int s1 = 0;
+                int s2 = 0;
+
+                for(int j = 0; j < 100; j++){
+                    s1 += arr[i][j];
+                    s2 += arr[j][i];
+                }
+                list.add(s1);
+                list.add(s2);
+
+
+                cross += arr[i][i];
+            }
+
+            list.add(cross);
+
+            Collections.sort(list);
+
+            int result = list.get(list.size()-1);
+
+            System.out.println("#" + test_case + " " + result);
+
+        }
+    }
+}
