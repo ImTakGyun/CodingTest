c5c119023eb1f627fd3ab459a9b06172cf775617
TakGyun <gyun1712@gmail.com>
2023-12-26 22:02:38 +0900

    feat: 다이나믹 프로그래밍 복습-실전_바닥공사

--- algorithm/DynamicProgramming/Practice/실전_바닥공사.java
+++ algorithm/DynamicProgramming/Practice/실전_바닥공사.java
@@ -18,18 +18,16 @@ public class 실전_바닥공사 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n+1];
-        int[] div = new int[n+1];
+        int[] dp = new int[n+1];
 
-        arr[1] = 1;
-        arr[2] = 3;
+        dp[1] = 1;
+        dp[2] = 3;
 
-        for(int i =3; i < n+1; i++){
-            arr[i] = arr[i-1] + 2 * arr[i-2];
-            div[i] = arr[i] % 796796;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2] * 2;
         }
 
-        System.out.println(div[n]);
+        System.out.println(dp[n] % 796796);
     }
 
     public static void main(String[] args) throws IOException {
